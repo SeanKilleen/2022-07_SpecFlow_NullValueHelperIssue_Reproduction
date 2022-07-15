4db1c89df080d652bbcb6a0fdcea0fc4cd4a14d0
Sean Killeen <SeanKilleen@gmail.com>
2022-07-15 12:27:40 -0400

    Initial setup

---
+++ SpecFlowTests/TestFeature.feature
@@ -0,0 +1,5 @@
+Feature: Thing
+
+Scenario: Test Null String Value
+    Given I have a string with a value of <null>
+    Then the string should be null
---
+++ SpecFlowTests/TestFeatureSteps.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTests;
+
+[Binding]
+public class TestFeatureSteps
+{
+    private string _myString;
+
+    [Given("I have a string with a value of (.*)")]
+    public void GivenIHaveAWithAValueOf(string theValue)
+    {
+        _myString = theValue;
+    }
+
+    [Then("the string should be null")]
+    public void TheStringShouldBeNull()
+    {
+        _myString.Should().BeNull();
+    }
+
+}
